5c185baf96c7d36811c1bd2f8ad3918f7eb585d5
magento <magento.guru@yahoo.com>
2025-06-04 19:34:00 +0800

    Re-add build/bin

--- langchain/start.sh
+++ langchain/start.sh
@@ -12,6 +12,8 @@
 
 # Start llama.cpp model server
 #!/bin/sh
+mkdir -p ./llama.cpp/models
+curl -L https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf  -o ./llama.cpp/model/tinyllama.gguf
 
 # Start llama.cpp model server
 chmod +x ./llama.cpp/build/bin/llama-cli
