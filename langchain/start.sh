1d9f6ff8b89d7b4ac1a13e956844a82a976d9a10
magento <magento.guru@yahoo.com>
2025-06-05 22:15:57 +0800

    adding gpt2.gguf

--- langchain/start.sh
+++ langchain/start.sh
@@ -17,16 +17,26 @@ mkdir -p ./llama.cpp/models
 # Create necessary directories
 mkdir -p ./llama.cpp/build/bin
 
-# Download prebuilt llama-server binary for Linux x86_64
-# curl -L https://huggingface.co/morepaxos/llama.cpp/resolve/main/server/linux_x86_64/llama-server  \
-#   -o ./llama.cpp/build/bin/llama-server
+MODEL_DIR="llama.cpp/models"
+MODEL_URL="https://drive.google.com/uc?id=1RPGR7DFcZMl4w5FstmNTI_BLZ0Fcm4Ji&export=download"
+MODEL_PATH="$MODEL_DIR/gpt2.gguf"
 
+# Create models directory 
+mkdir -p "$MODEL_DIR"
 
+# Download if not already downloaded
+if [ ! -f "$MODEL_PATH" ]; then
+    echo "Downloading GGUF model..."
+    wget --quiet --continue --output-document="$MODEL_PATH" "$MODEL_URL"
+    echo "Model saved to $MODEL_PATH"
+else
+    echo "Model already exists. Skipping download."
+fi
 
-curl -L https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf  -o ./llama.cpp/models/tinyllama.gguf
+# curl -L https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf  -o ./llama.cpp/models/tinyllama.gguf
 
 # Start llama.cpp model server
-python -m llama_cpp.server --model ./llama.cpp/models/tinyllama.gguf --host 0.0.0.0 --port 8000
+python -m llama_cpp.server --model ./llama.cpp/models/gpt2.gguf --host 0.0.0.0 --port 8000
 
 # Start LiteLLM proxy
 # litellm --port 4000 --model custom_openai/llama-cpp --api_base http://localhost:8000 &
