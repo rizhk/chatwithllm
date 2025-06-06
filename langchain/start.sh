ae65d69203b8bf6da77c67fbc3af78717cb1063d
magento <magento.guru@yahoo.com>
2025-06-06 12:54:24 +0800

    removed llama server

--- langchain/start.sh
+++ langchain/start.sh
@@ -1,23 +1,9 @@
-# mkdir -p ~/mamba-postgres/data
-# initdb -D ~/mamba-postgres/data
-# micromamba activate torch_env
-# fastapi dev main.py
-# pg_ctl -D ~/mamba-postgres/data -l ~/mamba-postgres/logfile start
-# ollama run --model gemma:2b --port 11434 --host
 
-# # litellm --model ollama/gemma:2b --api_base http://127.0.0.1:11434 --host 127.0.0.1
-# sudo litellm --config config.yaml --debug # somehow this works with sudo only
-
-#!/bin/sh
-
-# Start llama.cpp model server
 #!/bin/sh
 mkdir -p ./llama.cpp/models
 
 # Create necessary directories
 mkdir -p ./llama.cpp/build/bin
-
-#!/bin/bash
 
 
 MODEL_DIR="llama.cpp/models"
@@ -37,9 +23,9 @@ else
 fi
 
 # Start llama.cpp server
-echo "Starting llama.cpp server with model at $MODEL_PATH..."
-python -m llama_cpp.server --model "$MODEL_PATH" --host 0.0.0.0 --port 8000
-echo "started llama.cpp server with model at $MODEL_PATH..."
+# echo "Starting llama.cpp server with model at $MODEL_PATH..."
+# python -m llama_cpp.server --model "$MODEL_PATH" --host 0.0.0.0 --port 8000
+# echo "started llama.cpp server with model at $MODEL_PATH..."
 
 # Wait for server to start
 SERVER_PID=$!
@@ -52,14 +38,6 @@ for i in {1..10}; do
     }
     sleep 1
 done
-
-# curl -L https://huggingface.co/TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF/resolve/main/tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf  -o ./llama.cpp/models/tinyllama.gguf
-
-# Start llama.cpp model server
-# python -m llama_cpp.server --model ./llama.cpp/models/gpt2.gguf --host 0.0.0.0 --port 8000
-
-# Start LiteLLM proxy
-# litellm --port 4000 --model custom_openai/llama-cpp --api_base http://localhost:8000 &
 
 # Start FastAPI app
 uvicorn main:app --host 0.0.0.0 --port 10000 --reload
